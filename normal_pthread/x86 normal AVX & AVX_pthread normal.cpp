50001ae4c1156ad0fdac3e8b952545f62625e7f0
lhw <93196393+jtysxtm@users.noreply.github.com>
2023-05-09 16:01:35 +0800

    Add files via upload

---
+++ normal_pthread/x86 normal AVX & AVX_pthread normal.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <immintrin.h>
+#include<windows.h>
+#include<cstdlib>
+using namespace std;
+#pragma comment(lib, "pthreadVC2.lib")
+
+//------------------------------------------ 线程控制变量 ------------------------------------------
+typedef struct
+{
+    int t_id;
+} threadParam_t;
+
+sem_t sem_Division;
+pthread_barrier_t barrier;
+
+const int THREAD_NUM = 8;
+
+// ------------------------------------------ 全局计算变量 ------------------------------------------
+#define N 2500
+const int LOOP = 20;
+float A[N][N];
+float matrix[N][N];
+
+
+// ------------------------------------------ AVX 并行算法 ------------------------------------------
+void calculate_AVX()
+{
+    for (int k = 0; k < N; k++)
+    {
+        // float Akk = matrix[k][k];
+        __m256 Akk = _mm256_set1_ps(matrix[k][k]);
+        int j;
+        // 并行处理
+        for (j = k + 1; j + 7 < N; j += 8)
+        {
+            // float Akj = matrix[k][j];
+            __m256 Akj = _mm256_loadu_ps(matrix[k] + j);
+            // Akj = Akj / Akk;
+            Akj = _mm256_div_ps(Akj, Akk);
+            // Akj = matrix[k][j];
+            _mm256_storeu_ps(matrix[k] + j, Akj);
+        }
+        // 串行处理结尾
+        for (; j < N; j++)
+        {
+            matrix[k][j] = matrix[k][j] / matrix[k][k];
+        }
+        matrix[k][k] = 1;
+        for (int i = k + 1; i < N; i++)
+        {
+            // float Aik = matrix[i][k];
+            __m256 Aik = _mm256_set1_ps(matrix[i][k]);
+            for (j = k + 1; j + 7 < N; j += 8)
+            {
+                // float Akj = matrix[k][j];
+                __m256 Akj = _mm256_loadu_ps(matrix[k] + j);
+                // float Aij = matrix[i][j];
+                __m256 Aij = _mm256_loadu_ps(matrix[i] + j);
+                // AikMulAkj = matrix[i][k] * matrix[k][j];
+                __m256 AikMulAkj = _mm256_mul_ps(Aik, Akj);
+                // Aij = Aij - AikMulAkj;
+                Aij = _mm256_sub_ps(Aij, AikMulAkj);
+                // matrix[i][j] = Aij;
+                _mm256_storeu_ps(matrix[i] + j, Aij);
+            }
+            // 串行处理结尾
+            for (; j < N; j++)
+            {
+                matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+            }
+            matrix[i][k] = 0;
+        }
+    }
+}
+
+// ------------------------------------------ pthread_AVX 线程函数 ------------------------------------------
+void* threadFunc_AVX(void* param)
+{
+    threadParam_t* thread_param_t = (threadParam_t*)param;
+    int t_id = thread_param_t->t_id;
+    for (int k = 0; k < N; k++)
+    {
+        // 如果当前是0号线程，则进行除法操作，其余线程处于等待状态
+        if (t_id == 0)
+        {
+            // float Akk = matrix[k][k];
+            __m256 Akk = _mm256_set1_ps(matrix[k][k]);
+            int j;
+            //考虑对齐操作
+            for (j = k + 1; j + 7 < N; j += 8)
+            {
+                // float Akj = matrix[k][j];
+                __m256 Akj = _mm256_loadu_ps(matrix[k] + j);
+                // Akj = Akj / Akk;
+                Akj = _mm256_div_ps(Akj, Akk);
+                // Akj = matrix[k][j];
+                _mm256_storeu_ps(matrix[k] + j, Akj);
+            }
+            for (; j < N; j++)
+            {
+                matrix[k][j] = matrix[k][j] / matrix[k][k];
+            }
+            matrix[k][k] = 1.0;
+        }
+        else
+        {
+            sem_wait(&sem_Division);
+        }
+
+        // 除法操作完成后，如果是0号线程，则需要唤醒其他线程
+        if (t_id == 0)
+        {
+            for (int i = 1; i < THREAD_NUM; i++)
+            {
+                sem_post(&sem_Division);
+            }
+        }
+        else
+        {
+            // 循环划分任务
+            for (int i = k + t_id; i < N; i += (THREAD_NUM - 1))
+            {
+                // float Aik = matrix[i][k];
+                __m256 Aik = _mm256_set1_ps(matrix[i][k]);
+                int j = k + 1;
+                for (; j + 7 < N; j += 8)
+                {
+                    // float Akj = matrix[k][j];
+                    __m256 Akj = _mm256_loadu_ps(matrix[k] + j);
+                    // float Aij = matrix[i][j];
+                    __m256 Aij = _mm256_loadu_ps(matrix[i] + j);
+                    // AikMulAkj = matrix[i][k] * matrix[k][j];
+                    __m256 AikMulAkj = _mm256_mul_ps(Aik, Akj);
+                    // Aij = Aij - AikMulAkj;
+                    Aij = _mm256_sub_ps(Aij, AikMulAkj);
+                    // matrix[i][j] = Aij;
+                    _mm256_storeu_ps(matrix[i] + j, Aij);
+                }
+                for (; j < N; j++)
+                {
+                    matrix[i][j] = matrix[i][j] - matrix[i][k] * matrix[k][j];
+                }
+                matrix[i][k] = 0;
+            }
+        }
+
+        // 所有线程准备进入下一轮
+        pthread_barrier_wait(&barrier);
+    }
+    pthread_exit(NULL);
+    return NULL;
+}
+
+// ------------------------------------------ pthread_AVX 并行算法 ------------------------------------------
+void calculate_pthread_AVX()
+{
+    // 信号量初始化
+    sem_init(&sem_Division, 0, 0);
+    pthread_barrier_init(&barrier, NULL, THREAD_NUM);
+
+    // 创建线程
+    pthread_t threads[THREAD_NUM];
+    threadParam_t thread_param_t[THREAD_NUM];
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
+        thread_param_t[i].t_id = i;
+        pthread_create(&threads[i], NULL, threadFunc_AVX, (void*)(&thread_param_t[i]));
+    }
+
+    // 加入执行线程
+    for (int i = 0; i < THREAD_NUM; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    // 销毁信号量
+    sem_destroy(&sem_Division);
+    pthread_barrier_destroy(&barrier);
+}
+
+
+void init_data()
+{
+    for (int i = 0; i < N; i++)
+    {
+        A[i][i] = 1.0;
+        for (int j = i + 1; j < N; j++)
+            A[i][j] = rand();
+    }
+    for (int k = 0; k < N; k++)
+        for (int i = k + 1; i < N; i++)
+            for (int j = 0; j < N; j++)
+                A[i][j] += A[k][j];
+}
+
+// 用data初始化matrix，保证每次进行计算的数据是一致的
+void init_matrix()
+{
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            matrix[i][j] = A[i][j];
+}
+
+int main()
+{
+    init_data();
+    long long head, tail, freq;
+
+    // ====================================== AVX ======================================
+
+    // similar to CLOCKS_PER_SEC
+    QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    // start time
+    QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    for (int i = 0; i < LOOP; i++)
+    {
+        init_matrix();
+        calculate_AVX();
+    }
+    QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+    cout << (tail - head) * 1000.0 / (freq*LOOP) << endl;
+
+    // ====================================== pthread_AVX ======================================
+    
+    //// similar to CLOCKS_PER_SEC
+    //QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+    //// start time
+    //QueryPerformanceCounter((LARGE_INTEGER*)&head);
+    //for (int i = 0; i < LOOP; i++)
+    //{
+    //    init_matrix();
+    //    calculate_pthread_AVX();
+    //}
+    //QueryPerformanceCounter((LARGE_INTEGER*)&tail);
+    //cout<< (tail - head) * 1000.0 / (freq * LOOP) << endl;
+
+    //==========================================================================================
+
+    system("pause");
+}
